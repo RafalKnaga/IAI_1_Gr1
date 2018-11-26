e9040e96f133f9cce8e999680d1fe408d5495505
Andrzej <andreq114@gmail.com>
2018-11-26 17:17:51 +0100

    Kolokwium

---
+++ navbar.js
@@ -0,0 +1,10 @@
+let nav = '<nav>\n' +
+    '<h1>Nawigacja</h1>' +
+    '<li><a href="index.html">Główna</a></li>' +
+    '<li><a href="kontakt.html">Kontakt</a></li>'+
+    '</nav>';
+
+
+let navBar = document.querySelector(".nav-bar");
+
+navBar.innerHTML=nav;
